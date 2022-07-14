4ddb822c1ae61acf7e0c103dda496e3f433e4a7b
Simon Chabrier <68445270+SimonChabrier@users.noreply.github.com>
2022-07-14 23:32:13 +0200

    Revert "Feature/fixture"

--- config/bundles.php
+++ config/bundles.php
@@ -15,5 +15,4 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
